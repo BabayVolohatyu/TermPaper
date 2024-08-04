68373efe052ab23d8fce9be8c17abe9e662d984d
BabayVolohatyu <babayvolohatyu@gmail.com>
2024-08-04 19:09:32 +0300

    Delete visual function. From now this is abstract class for further polymorphism-based functions

--- ReferenceBook/Serializable.h
+++ ReferenceBook/Serializable.h
@@ -4,9 +4,6 @@
 
 class Serializable {
 public:
-	virtual void downloadInfo(const std::string& path) = 0;
-	
-	virtual void uploadInfo(const std::string& path) const = 0;
 
 	virtual ~Serializable() = default;
 };
