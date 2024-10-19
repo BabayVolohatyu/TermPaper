5f67a21ff05c63978f755648b3707cc210e8fedf
BabayVolohatyu <babayvolohatyu@gmail.com>
2024-10-19 20:58:58 +0300

    Add TimeButton class for time block

---
+++ ReferenceBook/UI/Headers/TimeButton.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Button.h"
+
+class TimeButton : public Button {
+private:
+    std::string preciseTime;
+
+    TimeButton() = delete;
+
+public:
+    TimeButton(const std::string &name, int height, int width);
+
+    void setPreciseTime(const std::string &preciseTime);
+
+    void print() const;
+};
---
+++ ReferenceBook/UI/Source/TimeButton.cpp
@@ -0,0 +1,67 @@
+#include "../Headers/TimeButton.h"
+#include "../Headers/ConsoleManager.h"
+TimeButton::TimeButton(const std::string &name, int height, int width) :
+Button{name, height, width}{}
+
+void TimeButton::print() const {
+    ConsoleManager::changeTextColor(currentColor);
+    std::cout << ' ';
+    for (int i = 0; i < width; i++) {
+        std::cout << '~';
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < height / 2; i++) {
+        std::cout << '|';
+        for (int j = 0; j < width; j++) {
+            std::cout << ' ';
+        }
+        std::cout << '|';
+        std::cout << std::endl;
+    }
+
+    int offset = (width - name.size()) / 2;
+    if (offset == 0) offset++;
+    std::cout << '|';
+    for (int i = 0; i < offset; i++) {
+        std::cout << ' ';
+    }
+    std::cout << name;
+    for (int i = 0; i < offset; i++) {
+        std::cout << ' ';
+    }
+    std::cout << '|';
+    std::cout << std::endl;
+
+    offset = (width - preciseTime.size()) / 2;
+    if (offset == 0) offset++;
+    std::cout << '|';
+    for (int i = 0; i < offset; i++) {
+        std::cout << ' ';
+    }
+    std::cout << preciseTime;
+    for (int i = 0; i < offset; i++) {
+        std::cout << ' ';
+    }
+    std::cout << '|';
+    std::cout << std::endl;
+
+    for (int i = 0; i < height / 2; i++) {
+        std::cout << '|';
+        for (int j = 0; j < width; j++) {
+            std::cout << ' ';
+        }
+        std::cout << '|';
+        std::cout << std::endl;
+    }
+
+    std::cout << ' ';
+    for (int i = 0; i < width; i++) {
+        std::cout << '~';
+    }
+    std::cout << std::endl;
+}
+
+void TimeButton::setPreciseTime(const std::string &preciseTime) {
+    this->preciseTime = preciseTime;
+}
+
