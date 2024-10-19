8a02c1262f4abbd64d33103c8d324df44dd1af22
BabayVolohatyu <babayvolohatyu@gmail.com>
2024-10-19 20:57:05 +0300

    Add TimeManager class

---
+++ ReferenceBook/System/Headers/TimeManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <mutex>
+
+
+#include "Date.h"
+#include "../../UI/Headers/TimeButton.h"
+
+class TimeManager {
+    private:
+    static std::mutex mutex;
+    static std::chrono::time_point<std::chrono::system_clock> currentTime;
+    TimeManager() = delete;
+    public:
+
+    static void startClockUpdateThread(TimeButton& clockButton);
+};
+
---
+++ ReferenceBook/System/Source/TimeManager.cpp
@@ -0,0 +1,23 @@
+#include <thread>
+#include "../Headers/TimeManager.h"
+#include "../../UI/Headers/TimeButton.h"
+
+std::mutex TimeManager::mutex;
+std::chrono::time_point<std::chrono::system_clock> TimeManager::currentTime = Date::getLocalTime();
+
+void TimeManager::startClockUpdateThread(TimeButton& timeButton) {
+    std::thread clockUpdateThread([&timeButton] {
+        while (true) {
+            std::unique_lock uniqueLock{mutex};
+            currentTime = Date::getLocalTime();
+            timeButton.setName(Date::parseTimePointToString(currentTime));
+            timeButton.setPreciseTime(Date::getLocalTimeOfTheDayAsString());
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+    clockUpdateThread.detach();
+}
+
+
+
+
