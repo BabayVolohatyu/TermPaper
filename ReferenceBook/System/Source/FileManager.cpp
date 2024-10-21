5328f5b7f0f5bd8b126e6c021b39abcfdde6124c
BabayVolohatyu <babayvolohatyu@gmail.com>
2024-10-21 15:14:47 +0300

    Udate class functions

--- ReferenceBook/System/Source/FileManager.cpp
+++ ReferenceBook/System/Source/FileManager.cpp
@@ -1,16 +1,27 @@
 #include <fstream>
+#include <filesystem>
 
 #include "../Headers/FileManager.h"
 #include "../Headers/Serializable.h"
 
 void FileManager::uploadToFile(const std::string& path, const Serializable& object) {
+
+	std::filesystem::path dirPath = std::filesystem::path(path).parent_path();
+	if (!exists(dirPath)) create_directories(dirPath);
+
 	std::ofstream ofs{ path, std::ofstream::out };
+
 	ofs << object;
 	ofs.close();
 }
 
 void FileManager::downloadFromFile(const std::string& path, Serializable& object){
+
+	std::filesystem::path dirPath = std::filesystem::path(path).parent_path();
+	if (!exists(dirPath)) create_directories(dirPath);
+
 	std::ifstream ifs{ path, std::ifstream::in };
+
 	ifs >> object;
 	ifs.close();
 }
