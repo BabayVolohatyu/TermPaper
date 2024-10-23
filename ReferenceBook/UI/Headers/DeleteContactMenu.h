c05214007c97d2da6ef6c982e8cbb908f1161edf
BabayVolohatyu <babayvolohatyu@gmail.com>
2024-10-23 14:02:20 +0300

    add delete contact button

---
+++ ReferenceBook/UI/Headers/DeleteContactMenu.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Menu.h"
+
+class DeleteContactMenu : public Menu {
+public:
+    DeleteContactMenu();
+
+    ~DeleteContactMenu() = default;
+
+    void print() const override;
+};
---
+++ ReferenceBook/UI/Source/DeleteContactMenu.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include "../Headers/DeleteContactMenu.h"
+
+#include "../../System/Headers/Contact.h"
+#include "../../System/Headers/ContactBook.h"
+#include "../Headers/ConsoleManager.h"
+#include "../Headers/ContactMenu.h"
+
+DeleteContactMenu::DeleteContactMenu(): Menu{"Deleting contact", 0} {}
+
+void DeleteContactMenu::print() const {
+    ConsoleManager::setIgnoreInputStatus(true);
+    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+    std::string userInput;
+    std::cout << '|' << name << '|' << std::endl;
+    std::cout << '|' << "Enter the name of contact you want to delete:";
+    std::getline(std::cin, userInput);
+    int sizeOfBook = ContactBook::getInstance()->getSize();
+    for(int i = 0; i < sizeOfBook ; i++) {
+        if(userInput == ContactBook::getInstance()->getContact(i)->getName()) {
+            ContactBook::getInstance()->erase(i);
+        }
+    }
+    int sizeOfContactBookMenu = ContactMenu::getInstance()->getSize();
+    for(int i = 0; i < sizeOfContactBookMenu ; i++) {
+        if(userInput == ContactMenu::getInstance()->getButton(i)->getName()) {
+            ContactMenu::getInstance()->erase(i);
+        }
+    }
+    ConsoleManager::returnToPreviousMenu();
+    ConsoleManager::clear();
+    ConsoleManager::display(ConsoleManager::getCurrentMenu());
+    ConsoleManager::setIgnoreInputStatus(false);
+}
+
